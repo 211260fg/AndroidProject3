702b3c667af6fa41edd5912f6481253dd24e2434
Daan Flore <daan.flore.u8852@student.hogent.be>
2015-10-14 10:33:01 +0200

    domein opstellen

---
+++ app/src/main/java/com/example/floriangoeteyn/androidproject3/domein/DomeinController.java
@@ -0,0 +1,7 @@
+package com.example.floriangoeteyn.androidproject3.domein;
+
+/**
+ * Created by daan on 14/10/2015.
+ */
+public class DomeinController {
+}
---
+++ app/src/main/java/com/example/floriangoeteyn/androidproject3/persistentie/PersistentieController.java
@@ -0,0 +1,9 @@
+package com.example.floriangoeteyn.androidproject3.persistentie;
+
+/**
+ * Created by daan on 14/10/2015.
+ */
+public class PersistentieController {
+    //test
+
+}
