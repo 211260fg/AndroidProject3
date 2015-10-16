430b4bd113082cc2c3674345b3e57da41a61678e
RobinLodrigo <robin.lodrigo.u8341@student.hogent.be>
2015-10-16 10:44:07 +0200

    Splash screen aanpassingen

---
+++ app/src/main/java/com/example/floriangoeteyn/androidproject3/Splash.java
@@ -0,0 +1,42 @@
+package com.example.floriangoeteyn.androidproject3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * Created by Robin on 16/10/2015.
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
