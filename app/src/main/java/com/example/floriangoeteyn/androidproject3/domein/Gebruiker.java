b5a41463b66e2d092558d9ec6e546c0500fb1fd6
Jeroen Hoste <jeroen.hoste.u8020@student.hogent.be>
2015-11-05 03:28:42 +0100

    facebook login setup klaar (todo: linken met local login)

---
+++ app/src/main/java/com/example/floriangoeteyn/androidproject3/domein/Gebruiker.java
@@ -0,0 +1,7 @@
+package com.example.floriangoeteyn.androidproject3.domein;
+
+/**
+ * Created by Jeroen on 4/11/2015.
+ */
+public class Gebruiker {
+}
---
+++ app/src/main/java/com/example/floriangoeteyn/androidproject3/persistentie/FacebookGraphAPI.java
@@ -0,0 +1,7 @@
+package com.example.floriangoeteyn.androidproject3.persistentie;
+
+/**
+ * Created by Jeroen on 4/11/2015.
+ */
+public class FacebookGraphAPI {
+}
